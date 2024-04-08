4c0270848561f016d4006892234fd2a521ecb535
curiousKidd <skasrjhaan@gmail.com>
2024-04-08 23:12:42 +0900

    first commit

---
+++ src/main/kotlin/com/example/kotiln/KotilnApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotiln
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotilnApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotilnApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotiln/KotilnApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotiln
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotilnApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
