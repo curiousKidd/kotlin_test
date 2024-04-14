db443f74a4c2d50d5e5edc9fa9265056c8a3ac32
curiousKidd <skasrjhaan@gmail.com>
2024-04-14 23:12:24 +0900

    add TDD

---
+++ src/test/kotlin/com/example/kotiln/VariableTest.kt
@@ -0,0 +1,24 @@
+package com.example.kotiln
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VariableTest {
+
+    @Test
+    fun variableExam() {
+        val message: String
+
+        val b = true
+
+        message = if (b) {
+            "true"
+        } else {
+            "false"
+        }
+
+        Assertions.assertTrue(message == "true")
+    }
+}
