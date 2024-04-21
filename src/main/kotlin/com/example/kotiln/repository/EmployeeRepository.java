706b3305854e987bcfed1c53da8d57e5f142779a
curiousKidd <skasrjhaan@gmail.com>
2024-04-21 22:57:38 +0900

    20240421

---
+++ src/main/kotlin/com/example/kotiln/repository/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package com.example.kotiln.repository;
+
+import com.example.kotiln.model.entity.Employee;
+import feign.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, String> {
+
+    @Query(nativeQuery = true, value = "SELECT e.* " +
+            "FROM Employees e " +
+            "ORDER BY e.emp_no DESC " +
+            "LIMIT :limit "
+    )
+    List<Employee> getEmployees(@Param("limit") int limit);
+}
