1a7ac94e6158a152c7c505a309db5cd24eb9c3a2
curiousKidd <skasrjhaan@gmail.com>
2024-04-25 23:42:13 +0900

    20240425

--- src/main/kotlin/com/example/kotlin/service/impl/KotlinTestServiceImpl.kt
+++ src/main/kotlin/com/example/kotlin/service/impl/KotlinTestServiceImpl.kt
@@ -1,15 +1,42 @@
 package com.example.kotlin.service.impl
 
 import com.example.kotlin.model.DTO.response.VaultResponseDTO
+import com.example.kotlin.model.DTO.resquest.VaultRequestDTO
 import com.example.kotlin.service.TestService
+import org.springframework.beans.factory.annotation.Value
 
 class KotlinTestServiceImpl : TestService {
 
+    @Value("\${spring.cloud.vault.app-role.role-id}")
+    private val roleId: String? = null
+
+    @Value("\${spring.cloud.vault.app-role.secret-id}")
+    private val secretId: String? = null
+
+
     override fun getAuthToken(): VaultResponseDTO {
         TODO("Not yet implemented")
+        val vaultRequestDTO = VaultRequestDTO.builder()
+            .role_id(roleId)
+            .secret_id(secretId)
+            .build()
+
+        return vaultFeignClient.getAuthToken(vaultRequestDTO)
     }
 
     override fun getSecretData(): VaultResponseDTO {
         TODO("Not yet implemented")
+
+        val vaultRequestDTO = VaultRequestDTO.builder()
+            .role_id(roleId)
+            .secret_id(secretId)
+            .build()
+
+        val token: VaultResponseDTO = vaultFeignClient.getAuthToken(vaultRequestDTO)
+        val client_token = token.auth.client_token
+
+        val secretData: VaultResponseDTO = vaultFeignClient.getSecretData(client_token)
+
+        return secretData
     }
 }
